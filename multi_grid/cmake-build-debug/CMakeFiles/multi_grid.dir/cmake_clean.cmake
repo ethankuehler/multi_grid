0196b00ac82b3c5d3397df73812ab56502a7fe21
Ethan Kuehler <ethankuehler@gmail.com>
2019-11-05 16:21:13 +0000

    test

---
+++ multi_grid/cmake-build-debug/CMakeFiles/multi_grid.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multi_grid.dir/multi_grid.c.o"
+  "CMakeFiles/multi_grid.dir/operators.c.o"
+  "libmulti_grid.a"
+  "libmulti_grid.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/multi_grid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ multi_grid/cmake-build-debug/CMakeFiles/multi_grid.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libmulti_grid.a"
+)
